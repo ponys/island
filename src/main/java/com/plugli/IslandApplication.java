bab6285939206e2f7dfbc13944564d43592f2d28
Patricio Lugli <patricio.lugli@lithium.com>
2019-03-06 00:08:00 -0300

    UP-1 Initial commit

---
+++ src/main/java/com/plugli/IslandApplication.java
@@ -0,0 +1,13 @@
+package com.plugli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IslandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IslandApplication.class, args);
+	}
+
+}
